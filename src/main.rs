b287f77f3ee9dfc1932241210cc6ee281e163bd3
hamin <hamin.kwak@gi-vita.io>
2022-07-24 16:08:11 +0900

    러스트 프로그래밍 Ex2.3 예제

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let twenty = 20;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
+
+    let addition = twenty + twenty_one + twenty_two;
+
+    println!(
+        "{} + {} + {} = {}",
+        twenty, twenty_one, twenty_two, addition
+    );
+
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+
+    let forty_twos = [42.0, 42f32, 42.0_f32];
+    let forty_twos_f64 = [42.0, 42f64, 42.0_f64];
+
+    println!("{:02}", forty_twos[0]);
+    println!("{:02.3}", forty_twos[0]);
+    println!("{:09.3}", forty_twos[0]);
+    println!("{:09.6}", forty_twos[0]);
+    println!("{:09.10}", forty_twos[0]);
 }
