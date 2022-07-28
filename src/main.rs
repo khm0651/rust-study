0bdc4d64a1c8f19f43dbc259182d640ac5cb2368
hamin <hamin.kwak@gi-vita.io>
2022-07-28 22:27:36 +0900

    feat: add fun calc_calorie_from_elements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+const STARTING_BAGELS: i32 = 10;
+const READY_TO_SERVE_BAGELS: i32 = 2;
+
 fn main() {
+    let mut bagel = STARTING_BAGELS;
+    println!(
+        "ready to serve {} of {} bagels",
+        READY_TO_SERVE_BAGELS, bagel
+    );
+
+    println!("serving..!!");
+    bagel -= READY_TO_SERVE_BAGELS;
+    println!("now {} bagels left to serve", bagel);
+
     println!(
         "a bagel with {} calories",
         calc_calorie_from_elements(1.0, 1.0, 1.0)
