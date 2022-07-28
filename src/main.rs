44e04e40b76633f18323de5f291bd715bce6accd
hamin <hamin.kwak@gi-vita.io>
2022-07-28 22:00:54 +0900

    리팩토링 전 bagel, ready 출력

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let bagel = 10;
+    let ready = 2;
+    println!("ready to serve {} of {} bagels", ready, bagel);
+
+    println!("serving..!!");
+    let need = bagel;
+    println!("now {} bagels left to serve", need);
 }
