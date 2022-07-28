75d7b0f16e03088927b870804ea2331b8175cbe2
hamin <hamin.kwak@gi-vita.io>
2022-07-28 18:47:26 +0900

    러스트 프로그래밍 Ex2.21 예제

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let one = [1, 2, 3];
+    let two: [u8; 3] = [1, 2, 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    let arrays = [one, two, blank1, blank2];
+
+    for a in &arrays {
+        print!("{:?}: ", *a);
+        for n in a.iter() {
+            print!("\t{} + 10 = {}\t", *n, *n + 10);
+        }
+        println!();
+    }
 }
