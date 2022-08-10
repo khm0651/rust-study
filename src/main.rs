ff4c6c0400a84bb9ebc86d129697e8a5606460f5
hamin <hamin.kwak@gi-vita.io>
2022-08-11 01:15:41 +0900

    러스트 프로그래밍 Ex3.12 예제

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+struct File;
+
+trait Read {
+    fn read(self: &Self, save_to: &mut Vec<u8>) -> Result<usize, String>;
+}
+
+impl Read for File {
+    fn read(self: &Self, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        Ok(0)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let f = File {};
+    let mut buffer = vec![];
+    let n_bytes = f.read(&mut buffer).unwrap();
+    println!("{} byte(s) read from {:?}", n_bytes, f);
 }
