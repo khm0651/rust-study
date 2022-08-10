68190eda416db206fbc102a0a74a2642141e3b03
hamin <hamin.kwak@gi-vita.io>
2022-08-11 01:28:53 +0900

    러스트 프로그래밍 Ex3.15 예제

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::fmt::Result;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+enum FileState {
+    Open,
+    Close,
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            FileState::Open => {
+                write!(f, "OPEN")
+            }
+            FileState::Close => {
+                write!(f, "CLOSE")
+            }
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Close,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let f6 = File::new("f6.txt");
+    println!("{:?}", f6);
+    println!("{}", f6);
 }
