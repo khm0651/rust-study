29512d2e7b0a2ce9e14c24ebd97187ee9a139525
hamin <hamin.kwak@gi-vita.io>
2022-08-15 23:39:06 +0900

    ex4.1

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
 #[derive(Debug)]
-struct CubeSat {
-    id: u64,
-}
-
-#[derive(Debug)]
 enum StatusMessage {
     Ok,
 }
 
-/// 아래와 같이 함수에서 파라매터를 반환을 함으로써 소유권을 돌려 받을 수 있다.
-fn check_status(sat_id: CubeSat) -> CubeSat {
-    println!("sat_id: {:?}, status: {:?}", sat_id, StatusMessage::Ok);
-    sat_id
+fn check_status(sat_id: i64) -> StatusMessage {
+    StatusMessage::Ok
 }
 
 fn main() {
-    let sat_a = CubeSat { id: 0 };
-    let sat_b = CubeSat { id: 1 };
-    let sat_c = CubeSat { id: 2 };
+    /// 아래 로직이 성공 할 수 있는 이유는 원시타입은 Copy 트레이트를 구현하고 있기 때문에
+    /// 복제하지 않고서는 사용할 수 없을 때에 한해 복제가 되기 떄문에 아래 로직이 실행이 되는 것이다.
+    /// 원시타입은 '복사 의미'를 가지며 다른 타입은 '이동 의미'를 가진다.
+    let sat_a = 0;
+    let sat_b = 1;
+    let sat_c = 2;
 
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
-    // '대기 중' ...
-
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 }
