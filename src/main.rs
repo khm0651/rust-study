e00df941c517f6c55fec48ee46a319163ddf6d1a
hamin <hamin.kwak@gi-vita.io>
2022-08-16 00:08:55 +0900

    ex4.13

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,53 @@
 #[derive(Debug)]
+struct CubeSat {
+    id: i64,
+    mailbox: Mailbox,
+}
+
+impl CubeSat {
+    fn recv(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
+    }
+}
+
+#[derive(Debug)]
 enum StatusMessage {
     Ok,
 }
 
-fn check_status(sat_id: i64) -> StatusMessage {
-    StatusMessage::Ok
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
+}
+
+type Message = String;
+
+struct GroundStation;
+
+/// &self는 자신에 대한 읽기 전용 참조만 사용한다는 의미
+/// CueeSat 인스턴스의 읽기, 쓰기 소유권 요청 대신 대여를 요청한다. (쓰기를 요청하는 이유는 벡터의 값을 수정하는 메서드이기 때문에)
+/// msg는 Message인스턴스의 완전한 소유권을 요청한다.
+impl GroundStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
 }
 
 fn main() {
-    /// 아래 로직이 성공 할 수 있는 이유는 원시타입은 Copy 트레이트를 구현하고 있기 때문에
-    /// 복제하지 않고서는 사용할 수 없을 때에 한해 복제가 되기 떄문에 아래 로직이 실행이 되는 것이다.
-    /// 원시타입은 '복사 의미'를 가지며 다른 타입은 '이동 의미'를 가진다.
-    let sat_a = 0;
-    let sat_b = 1;
-    let sat_c = 2;
+    let base = GroundStation {};
+    let mut sat_a = CubeSat {
+        id: 0,
+        mailbox: Mailbox { messages: vec![] },
+    };
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    println!("t0: {:?}", sat_a);
 
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+    base.send(&mut sat_a, Message::from("hello hamin"));
+
+    println!("t1: {:?}", sat_a);
+
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+
+    print!("msg: {:?}", msg);
 }
