bfbca3cc56f5521171c89150faa6966de81b0cfb
hamin <hamin.kwak@gi-vita.io>
2022-08-16 20:48:21 +0900

    ex4.15

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
+use std::fmt::format;
+
 #[derive(Debug)]
 struct CubeSat {
-    id: i64,
-    mailbox: Mailbox,
+    id: u64,
 }
 
 impl CubeSat {
-    fn recv(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
+    fn recv(&self, mailBox: &mut Mailbox) -> Option<Message> {
+        mailBox.deliver(&self)
     }
-}
-
-#[derive(Debug)]
-enum StatusMessage {
-    Ok,
 }
 
 #[derive(Debug)]
@@ -20,34 +16,69 @@ struct Mailbox {
     messages: Vec<Message>,
 }
 
-type Message = String;
+impl Mailbox {
+    fn post(&mut self, msg: Message) {
+        self.messages.push(msg)
+    }
 
-struct GroundStation;
+    fn deliver(&mut self, recipient: &CubeSat) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                let msg = self.messages.remove(i);
+                return Some(msg);
+            }
+        }
+
+        None
+    }
+}
+
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
+}
+
+struct GroundStation {}
 
 /// &self는 자신에 대한 읽기 전용 참조만 사용한다는 의미
 /// CueeSat 인스턴스의 읽기, 쓰기 소유권 요청 대신 대여를 요청한다. (쓰기를 요청하는 이유는 벡터의 값을 수정하는 메서드이기 때문에)
 /// msg는 Message인스턴스의 완전한 소유권을 요청한다.
 impl GroundStation {
-    fn send(&self, to: &mut CubeSat, msg: Message) {
-        to.mailbox.messages.push(msg);
+    fn connect(&self, sat_id: u64) -> CubeSat {
+        CubeSat { id: sat_id }
+    }
+
+    fn send(&self, mailBox: &mut Mailbox, msg: Message) {
+        mailBox.post(msg)
     }
 }
 
+fn fetch_sat_ids() -> Vec<u64> {
+    vec![1, 2, 3]
+}
+
 fn main() {
+    let mut mail = Mailbox { messages: vec![] };
+
     let base = GroundStation {};
-    let mut sat_a = CubeSat {
-        id: 0,
-        mailbox: Mailbox { messages: vec![] },
-    };
 
-    println!("t0: {:?}", sat_a);
+    let sat_ids = fetch_sat_ids();
 
-    base.send(&mut sat_a, Message::from("hello hamin"));
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = Message {
+            to: sat.id,
+            content: format!("i'm hamin {}", sat.id),
+        };
+        base.send(&mut mail, msg)
+    }
 
-    println!("t1: {:?}", sat_a);
+    let sat_ids = fetch_sat_ids();
 
-    let msg = sat_a.recv();
-    println!("t2: {:?}", sat_a);
-
-    print!("msg: {:?}", msg);
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = sat.recv(&mut mail);
+        println!("{:?}: {:?}", sat, msg);
+    }
 }
