a77581e61f728ba63b3fc89ae062488eaa255559
hamin <hamin.kwak@gi-vita.io>
2022-09-06 22:29:59 +0900

    ex7.4

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
+use std::env;
+use std::fs::File;
 use std::io::Read;
 
 const BYTES_PER_LINE: usize = 16;
-const INPUT: &'static [u8] = br#"
+
 fn main() {
-    println!("Hello, world!");
-}"#;
+    let arg1 = env::args().nth(1);
+    let fname = arg1.expect("usage: fview FILENAME");
 
-fn main() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = vec![];
-    INPUT.read_to_end(&mut buffer)?;
+    let mut f = File::open(&fname).expect("Unable to open file.");
+    let mut pos = 0;
+    let mut buffer = [0; BYTES_PER_LINE];
 
-    let mut position_in_input = 0;
-    for line in buffer.chunks(BYTES_PER_LINE) {
-        print!("[0x{:08x}] ", position_in_input);
-        for byte in line {
-            print!("{:02x} ", byte);
+    while let Ok(_) = f.read_exact(&mut buffer) {
+        print!("[0x{:08x}] ", pos);
+
+        for byte in &buffer {
+            match *byte {
+                0x00 => print!(".  "),
+                0xff => print!("## "),
+                _ => print!("{:02x} ", byte),
+            }
         }
 
         println!();
-        position_in_input += BYTES_PER_LINE;
+        pos += BYTES_PER_LINE;
     }
-    Ok(())
 }
