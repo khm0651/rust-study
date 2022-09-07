ab1d5a2150829acd917150fc406b9607ffe5ff5f
hamin <hamin.kwak@gi-vita.io>
2022-09-07 21:35:16 +0900

    feat: add delete_user

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,5 +1,5 @@
 use crate::model::User;
-use actix_web::web::Json;
+use actix_web::web::{Json, Path};
 use actix_web::{HttpResponse, Responder};
 
 pub static mut USER_LIST: Vec<User> = vec![];
@@ -16,6 +16,22 @@ pub async fn post_user(user: Json<User>) -> impl Responder {
     let user = user.into_inner();
     unsafe { USER_LIST.push(user.clone()) }
     let result = serde_json::to_string(&user);
+    match result {
+        Ok(result) => HttpResponse::Ok().body(result),
+        Err(e) => HttpResponse::from_error(e),
+    }
+}
+
+pub async fn delete_user(user_id: Path<u16>) -> impl Responder {
+    let user_id = user_id.into_inner();
+    let remove_result: User;
+    unsafe {
+        let position = USER_LIST
+            .binary_search_by_key(&user_id, |user| user.user_id)
+            .expect("not found user");
+        remove_result = USER_LIST.remove(position)
+    }
+    let result = serde_json::to_string(&remove_result);
     match result {
         Ok(result) => HttpResponse::Ok().body(result),
         Err(e) => HttpResponse::from_error(e),
