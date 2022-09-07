b2f6313620f9ade1b02612935946b3016d699c61
hamin <hamin.kwak@gi-vita.io>
2022-09-07 21:23:38 +0900

    feat: add get_users integration test code

--- tests/api/user.rs
+++ tests/api/user.rs
@@ -23,6 +23,56 @@ async fn should_get_users_function_return_user_list_all_data() {
 }
 
 #[tokio::test]
+async fn should_get_users_function_return_size_is_2() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let dummy_data1 = User {
+        user_id: 10,
+        name: String::from("hamin"),
+        phone: String::from("010-4938-0651"),
+    };
+
+    let dummy_data2 = User {
+        user_id: 10,
+        name: String::from("hamin"),
+        phone: String::from("010-4938-0651"),
+    };
+
+    let post_user_response1 = client
+        .post(&format!("{}/{}/user", &app.address, API_VER))
+        .json(&dummy_data1)
+        .send()
+        .await
+        .expect("Failed to send post_user request");
+
+    let post_user_response2 = client
+        .post(&format!("{}/{}/user", &app.address, API_VER))
+        .json(&dummy_data2)
+        .send()
+        .await
+        .expect("Failed to send post_user request");
+
+    assert!(post_user_response1.status().is_success());
+    assert!(post_user_response2.status().is_success());
+
+    let get_users_response = client
+        .get(&format!("{}/{}/user", &app.address, API_VER))
+        .send()
+        .await
+        .expect("Failed to send get_users request");
+
+    assert!(get_users_response.status().is_success());
+
+    let result = get_users_response
+        .json::<Vec<User>>()
+        .await
+        .expect("parsing error");
+
+    assert_eq!(result.len(), 2)
+}
+
+#[tokio::test]
 async fn should_post_user_function_return_same_value() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
