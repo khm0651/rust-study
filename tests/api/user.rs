c8187aa7608c93b5dd535565d7ed8363b99ab4ca
hamin <hamin.kwak@gi-vita.io>
2022-09-08 17:24:37 +0900

    feat: add get_user integraion test code

--- tests/api/user.rs
+++ tests/api/user.rs
@@ -1,4 +1,4 @@
-use crate::helpers::spawn_app;
+use crate::helpers::{setting_two_dummy_data, spawn_app};
 use actix::api::USER_LIST;
 use actix::model::User;
 
@@ -8,6 +8,7 @@ static API_VER: &str = "api/v1";
 async fn should_get_users_function_if_success_return_user_list_all_data() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
+    setting_two_dummy_data();
 
     let response = client
         .get(&format!("{}/{}/user", &app.address, API_VER))
@@ -23,53 +24,24 @@ async fn should_get_users_function_if_success_return_user_list_all_data() {
 }
 
 #[tokio::test]
-async fn should_get_users_function_if_success_return_size_is_2() {
+async fn should_get_user_function_if_success_return_specific_user_data() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-
-    let dummy_data1 = User {
-        user_id: 10,
-        name: String::from("hamin"),
-        phone: String::from("010-4938-0651"),
-    };
-
-    let dummy_data2 = User {
-        user_id: 20,
-        name: String::from("hamin2"),
-        phone: String::from("010-5788-0651"),
-    };
-
-    let post_user_response1 = client
-        .post(&format!("{}/{}/user", &app.address, API_VER))
-        .json(&dummy_data1)
-        .send()
-        .await
-        .expect("Failed to send post_user request");
-
-    let post_user_response2 = client
-        .post(&format!("{}/{}/user", &app.address, API_VER))
-        .json(&dummy_data2)
-        .send()
-        .await
-        .expect("Failed to send post_user request");
-
-    assert!(post_user_response1.status().is_success());
-    assert!(post_user_response2.status().is_success());
-
-    let get_users_response = client
-        .get(&format!("{}/{}/user", &app.address, API_VER))
+    let dummy = setting_two_dummy_data();
+    let response = client
+        .get(&format!(
+            "{}/{}/user/{}",
+            &app.address, API_VER, dummy.user_id
+        ))
         .send()
         .await
         .expect("Failed to send get_users request");
 
-    assert!(get_users_response.status().is_success());
+    assert!(response.status().is_success());
 
-    let result = get_users_response
-        .json::<Vec<User>>()
-        .await
-        .expect("parsing error");
-
-    assert_eq!(result.len(), 2)
+    let result = response.json::<User>().await.expect("parsing error");
+    let expect = dummy.clone();
+    assert_eq!(result, expect);
 }
 
 #[tokio::test]
@@ -101,35 +73,14 @@ async fn should_post_user_function_if_success_return_same_value() {
 async fn should_delete_user_function_if_success_return_same_value() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-
-    let dummy_data = User {
-        user_id: 10,
-        name: String::from("hamin"),
-        phone: String::from("010-4938-0651"),
-    };
-
-    let post_user_response = client
-        .post(&format!("{}/{}/user", &app.address, API_VER))
-        .json(&dummy_data)
-        .send()
-        .await
-        .expect("Failed to send post_user request");
-
-    assert!(post_user_response.status().is_success());
-
-    let result = post_user_response
-        .json::<User>()
-        .await
-        .expect("parsing error");
-    let expect = dummy_data.clone();
-    assert_eq!(result, expect);
-
+    let dummy = setting_two_dummy_data();
+    let expect = dummy.clone();
     let delete_user_response = client
         .delete(&format!(
             "{}/{}/user/{}",
-            &app.address, API_VER, dummy_data.user_id
+            &app.address, API_VER, dummy.user_id
         ))
-        .json(&dummy_data)
+        .json(&dummy.clone())
         .send()
         .await
         .expect("Failed to send post_user request");
@@ -140,7 +91,7 @@ async fn should_delete_user_function_if_success_return_same_value() {
         .json::<User>()
         .await
         .expect("parsing error");
-    let expect = dummy_data.clone();
+
     assert_eq!(result, expect);
 }
 
@@ -148,42 +99,14 @@ async fn should_delete_user_function_if_success_return_same_value() {
 async fn should_delete_user_function_if_success_return_size_is_1() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-
-    let dummy_data1 = User {
-        user_id: 10,
-        name: String::from("hamin"),
-        phone: String::from("010-4938-0651"),
-    };
-
-    let dummy_data2 = User {
-        user_id: 20,
-        name: String::from("hamin2"),
-        phone: String::from("010-5788-0651"),
-    };
-
-    let post_user_response1 = client
-        .post(&format!("{}/{}/user", &app.address, API_VER))
-        .json(&dummy_data1)
-        .send()
-        .await
-        .expect("Failed to send post_user request");
-
-    let post_user_response2 = client
-        .post(&format!("{}/{}/user", &app.address, API_VER))
-        .json(&dummy_data2)
-        .send()
-        .await
-        .expect("Failed to send post_user request");
-
-    assert!(post_user_response1.status().is_success());
-    assert!(post_user_response2.status().is_success());
+    let dummy_data = setting_two_dummy_data();
 
     let delete_user_response = client
         .delete(&format!(
             "{}/{}/user/{}",
-            &app.address, API_VER, dummy_data1.user_id
+            &app.address, API_VER, dummy_data.user_id
         ))
-        .json(&dummy_data2)
+        .json(&dummy_data)
         .send()
         .await
         .expect("Failed to send get_users request");
@@ -204,4 +127,31 @@ async fn should_delete_user_function_if_success_return_size_is_1() {
         .expect("parsing error");
 
     assert_eq!(result.len(), 1)
+}
+
+#[tokio::test]
+async fn should_patch_user_function_if_success_return_patch_value() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    setting_two_dummy_data();
+
+    let dummy_data = User {
+        user_id: 20,
+        name: String::from("hamin343434"),
+        phone: String::from("010-5222222222222788-0651"),
+    };
+
+    let patch_user_response = client
+        .patch(&format!("{}/{}/user", &app.address, API_VER))
+        .json(&dummy_data)
+        .send()
+        .await
+        .expect("parsing error");
+
+    let result = patch_user_response
+        .json::<User>()
+        .await
+        .expect("parsing error");
+
+    assert_eq!(result, dummy_data)
 }
